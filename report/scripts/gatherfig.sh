44fc6f8993b297f06f2782743196a9148361b62a
Carl Christian Kjelgaard Mikkelsen <spock@cs.umu.se>
2024-05-07 17:00:33 +0200

    Manuscript is shaping up. :)

--- report/scripts/gatherfig.sh
+++ report/scripts/gatherfig.sh
@@ -1,3 +1,10 @@
 find experiments -name \*.png -exec cp {} fig \;
 find experiments -name \*.eps -exec cp {} fig \;
 
+# Crop white space from MATLAB eps files
+cd fig
+ps2pdf -DEPSCrop charmm36tol04.eps charmm36tol04.pdf
+ps2pdf -DEPSCrop charmm36tol12.eps charmm36tol12.pdf
+ps2pdf -DEPSCrop oplsaaltol04.eps oplsaaltol04.pdf
+ps2pdf -DEPSCrop oplsaaltol12.eps oplsaaltol12.pdf
+cd ..
